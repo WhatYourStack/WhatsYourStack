0f8f9579c96bb1412482b6258a9009207dd07fba
jay <daonj0715@gmail.com>
2023-12-06 17:30:23 +0900

    fix:dbClass

--- dbClass.py
+++ dbClass.py
@@ -1,13 +1,11 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
-import app
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress a warning
 
 db = SQLAlchemy(app)
 
@@ -16,27 +14,24 @@ class Member(db.Model):
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
+    boards = db.relationship('Board', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
 
 class Board(db.Model):
-    board_id = db.Column(db.Integer,primary_key=True)
-    comment_id = db.Column(db.Integer,db.ForeignKey('comment.comment_id'),nullable=True)
-    member_id = db.Column(db.Integer,db.ForeignKey('member.member_id'), nullable=True)
+    board_id = db.Column(db.Integer, primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member.member_id'), nullable=True)
     skill = db.Column(db.String, nullable=False)
     secondTag = db.Column(db.String, nullable=True)
     image_url = db.Column(db.String, nullable=True)
     content = db.Column(db.String, nullable=False)
-
-    db.relationship('Member')
-    db.relationship('Comment')
+    comments = db.relationship('Comment', backref='board', lazy=True)
 
 class Comment(db.Model):
     comment_id = db.Column(db.Integer, primary_key=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('member.member_id'),nullable=False)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'),nullable=False)
+    member_id = db.Column(db.Integer, db.ForeignKey('member.member_id'), nullable=False)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=False)
     content = db.Column(db.String, nullable=False)
-
-    db.relationship('Board')
-    db.relationship('Member')
     
+# Flask 앱 컨텍스트 안에서 데이터베이스를 생성합니다.
 with app.app_context():
     db.create_all()
