4aa4ec084ab808fd84781240f9d240ab2b9f8d3f
jay <daonj0715@gmail.com>
2023-12-06 16:20:32 +0900

    make table and input board

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import dbClass
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+      'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
+   return render_template('index.html')
+
+@app.route('/post/insert', methods=['POST'])
+def input_post():
+   photo = request.form['photoUrl']
+   skill = request.form['skill']
+   tags = request.form['tags']
+   content = request.form['content']
+
+   song = dbClass.Board(
+      comment_id = 1,
+      member_id =1,
+      skill=skill, 
+      secondTag=tags,
+      content=content, 
+      image_url=photo
+      )
+   db.session.add(song)
+   db.session.commit()
+
    return render_template('index.html')
 
 @app.route('/mypage')
@@ -10,4 +41,4 @@ def mypage():
 	return 'This is My page!'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(debug=True)
